78c105cbb776a6de7d21918e800df8cada9c53bc
Aml Abdelrhman <Aml586651@gmail.com>
2026-07-31 00:54:22 +0300

    Update Hero component

--- src/components/pages/Home/Hero.tsx
+++ src/components/pages/Home/Hero.tsx
@@ -18,7 +18,7 @@ export const Hero = () => {
     <>
       <section className="relative flex flex-col justify-between items-end w-full max-w-[1550px] mx-auto overflow-hidden px-6 sm:px-10 lg:px-[80px] pt-7 pb-[70px] min-h-[905px] md:min-h-screen lg:min-h-[1020px]">
         <div className="absolute inset-0 z-0 block overflow-hidden md:hidden">
-          <div className="relative w-full h-[130%] -top-[13%]">
+          <div className="relative w-full h-[111%] -top-[11%]">
             <Image
               src="/images/hero-mobile.svg"
               alt="Al-Ammar Family Heritage"
@@ -38,7 +38,7 @@ export const Hero = () => {
           />
         </div>
         <div
-          className="relative z-10 flex flex-col justify-end lg:justify-center items-start w-full max-w-[1280px] mx-auto my-auto pb-10 sm:pb-20 lg:pb-0 pt-12 sm:pt-28 lg:pt-0 gap-6 md:gap-12 lg:gap-[48px] min-h-[420px] sm:min-h-[600px] md:min-h-[700px] lg:min-h-[739px]"
+          className="relative z-10 flex flex-col justify-end lg:justify-center items-start w-full max-w-[1280px] mx-auto my-auto pb-10 sm:pb-20 lg:pb-0 pt-12 sm:pt-28 lg:pt-0 gap-6 md:gap-12 lg:gap-[48px] min-h-[480px] sm:min-h-[600px] md:min-h-[700px] lg:min-h-[739px]"
         >
           <div className="flex flex-col justify-center items-start w-full max-w-[764px] mx-auto lg:mx-0 text-right gap-2 min-h-[220px] sm:min-h-[300px] md:min-h-[350px] lg:min-h-[402px]">
             <div className="relative w-[160px] h-[80px] sm:w-[190px] sm:h-[95px] md:w-[229px] md:h-[116px]">
